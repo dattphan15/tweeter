9ec3c4c7ffd0e78363ef8cf8b04a751d6977f5c5
Kevin Phan <ken.phan@live.ca>
2021-02-24 03:56:02 +0000

    added red counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  console.log("We're connected!");
+
+  $('textarea').hover((event) => {
+    console.log('hovered over text area!', this);
+  });
+
+  $('#tweet-text').on("input", function() {
+    console.log($(this).siblings("div").find("output"));
+    let length = $(this).val().length;
+    let counter = $(this).siblings("div").find("output");
+    counter.html(140-length);
+    if (140 - length < 0) {
+      counter.addClass("turnRed");
+    }
+    else {
+      counter.removeClass("turnRed");
+    }
+  });
+
+  $('textarea').on("click", function() {
+    console.log(this);
+  });
+});
